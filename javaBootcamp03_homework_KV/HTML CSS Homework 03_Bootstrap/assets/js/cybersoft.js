27275e39e734637d3878f7ecc720de032f52135b
KhoiVu87 <38747766+khoivu87@users.noreply.github.com>
2023-09-09 12:02:56 +0700

    Add Event Listener & Mobile Navigation functions

--- javaBootcamp03_homework_KV/HTML CSS Homework 03_Bootstrap/assets/js/cybersoft.js
+++ javaBootcamp03_homework_KV/HTML CSS Homework 03_Bootstrap/assets/js/cybersoft.js
@@ -12,7 +12,40 @@
         return document.querySelector(el)
         }
     }
-  
+    
+    /**
+    * Easy event listener function
+    */
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+        if (all) {
+            selectEl.forEach(e => e.addEventListener(type, listener))
+        } else {
+            selectEl.addEventListener(type, listener)
+        }
+        }
+    }
+    
+    /**
+    * Mobile nav toggle
+    */
+    on('click', '.mobile-nav-toggle', function(e) {
+        select('#navbar').classList.toggle('navbar-mobile')
+        this.classList.toggle('bi-list')
+        this.classList.toggle('bi-x')
+    })
+
+    /**
+     * Mobile nav dropdowns activate
+     */
+    on('click', '.navbar .dropdown > a', function(e) {
+        if (select('#navbar').classList.contains('navbar-mobile')) {
+        e.preventDefault()
+        this.nextElementSibling.classList.toggle('dropdown-active')
+        }
+    }, true)
+    
     /**
      * Hero carousel indicators
      */
